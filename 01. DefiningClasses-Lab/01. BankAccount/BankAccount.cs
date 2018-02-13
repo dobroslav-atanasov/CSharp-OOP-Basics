69cd64679f0a3cade995f78f3e9ab3fd9e1fd7ee
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-02-13 09:30:11 +0200

    Upload: Defining Classes - Lab

---
+++ 01. DefiningClasses-Lab/01. BankAccount/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿public class BankAccount
+{
+    private int id;
+    private double balance;
+
+    public double Balance
+    {
+        get { return this.balance; }
+        set { this.balance = value; }
+    }
+
+    public int Id
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+}
---
+++ 01. DefiningClasses-Lab/02. BankAccountMethods/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Startup
+{
+    public static void Main()
+    {
+        BankAccount account = new BankAccount();
+        account.Id = 1;
+        account.Deposit(100);
+        account.Withdraw(25);
+
+        Console.WriteLine(account.ToString());
+    }
+}
