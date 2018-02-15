36b66e06915d908e765c38e7587ebab5f04ba970
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-02-15 08:59:46 +0200

    Upload: Defining Classes - Lab

---
+++ 01. DefiningClasses-Lab/03. TestClient/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿public class BankAccount
+{
+    private int id;
+    private decimal balance;
+
+    public decimal Balance
+    {
+        get { return this.balance; }
+        set { this.balance = value; }
+    }
+
+    public int Id
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        this.Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        this.Balance -= amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Account {this.Id}, balance {this.Balance}";
+    }
+}
