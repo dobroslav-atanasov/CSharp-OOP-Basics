32aa120d47ae7b25daa2653f3b64aa031adf4ee0
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-03-13 09:30:04 +0200

    Upload: Exam Preparation II - Avatar.

---
+++ 15.ExamPreparationII-Avatar/Avatar/Controller/Engine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Engine
+{
+    private NationsBuilder nationsBuilder;
+
+    public Engine()
+    {
+        this.nationsBuilder = new NationsBuilder();
+    }
+
+    public void Run()
+    {
+        string input = Console.ReadLine();
+        while (true)
+        {
+            List<string> inputParts = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            string command = inputParts[0];
+            switch (command)
+            {
+                case "Bender":
+                    this.nationsBuilder.AssignBender(inputParts);
+                    break;
+                case "Monument":
+                    this.nationsBuilder.AssignMonument(inputParts);
+                    break;
+                case "Status":
+                    Console.WriteLine(this.nationsBuilder.GetStatus(inputParts[1]));
+                    break;
+                case "War":
+                    this.nationsBuilder.IssueWar(inputParts[1]);
+                    break;
+                case "Quit":
+                    Console.WriteLine(this.nationsBuilder.GetWarsRecord());
+                    return;
+            }
+            input = Console.ReadLine();
+        }
+    }
+}
---
+++ 15.ExamPreparationII-Avatar/Avatar/Models/Benders/EarthBender.cs
@@ -0,0 +1,26 @@
+﻿public class EarthBender : Bender
+{
+    private double groundSaturation;
+    
+    public EarthBender(string name, int power, double groundSaturation) 
+        : base(name, power)
+    {
+        this.GroundSaturation = groundSaturation;
+    }
+
+    public double GroundSaturation
+    {
+        get { return this.groundSaturation; }
+        set { this.groundSaturation = value; }
+    }
+
+    public override double GetBenderPower()
+    {
+        return base.Power * this.GroundSaturation;
+    }
+
+    public override string ToString()
+    {
+        return $"Earth Bender: {this.Name}, Power: {this.Power}, Ground Saturation: {this.GroundSaturation:F2}";
+    }
+}
